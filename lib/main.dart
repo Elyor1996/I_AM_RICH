d09220e5857212e710cd0512ba63257293b14c88
Elyor1996 <elyor@bnpz.uz>
2022-01-31 06:42:28 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
+      backgroundColor: Colors.blueGrey[700],
       appBar: AppBar(
         title: Text('I AM RICH'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
       ),
     ),
   ));
